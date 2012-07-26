d2a346ab8486b0c241465ab76a9c03ebe83d73ba
Julien Delange <julien@gunnm.org>
2012-07-26 17:16:01 +0200

    Instructions almost completed !

--- Lost Runner/src/org/gunnm/lostrunner/graphics/InstructionsRenderer.java
+++ Lost Runner/src/org/gunnm/lostrunner/graphics/InstructionsRenderer.java
@@ -27,6 +27,9 @@ public class InstructionsRenderer implements Renderer
 	public final static int PAGE_UTILS = 4;
 	private LostIcon iconLogo;
 	private LostIcon bitmapInstructions1;
+	private LostIcon bitmapInstructions2;
+	private LostIcon bitmapInstructions3;
+	private LostIcon bitmapInstructions4;
 	
 	public InstructionsRenderer (Context c)
 	{
@@ -40,6 +43,9 @@ public class InstructionsRenderer implements Renderer
 		
 		iconLogo = new LostIcon (c, "logo.png", -1.5f, 3.5f, -3.0f, LostIcon.ICON_BIG);
 		bitmapInstructions1 = new LostIcon (c, "instructions1.png", 0.8f, -1.0f, -1.8f, LostIcon.ICON_BIG, LostIcon.FORM_RECTANGLE);
+		bitmapInstructions2 = new LostIcon (c, "instructionsgun.png", 0.8f, -1.0f, -1.8f, LostIcon.ICON_BIG, LostIcon.FORM_RECTANGLE);
+		bitmapInstructions3 = new LostIcon (c, "bomb.png", 0.8f, -1.0f, -1.8f, LostIcon.ICON_BIG, LostIcon.FORM_RECTANGLE);
+		bitmapInstructions4 = new LostIcon (c, "utils.png", 0.8f, -1.0f, -1.8f, LostIcon.ICON_BIG, LostIcon.FORM_RECTANGLE);
 		
 	}
 
@@ -68,6 +74,9 @@ public class InstructionsRenderer implements Renderer
 	      
 	      iconLogo.loadGLTexture(gl, context);
 	      bitmapInstructions1.loadGLTexture(gl, context);
+	      bitmapInstructions2.loadGLTexture(gl, context);
+	      bitmapInstructions3.loadGLTexture(gl, context);
+	      bitmapInstructions4.loadGLTexture(gl, context);
 	}
 	
 	public void onDrawFrame(GL10 gl) {
@@ -128,6 +137,208 @@ public class InstructionsRenderer implements Renderer
 	      
 	      gl.glPopMatrix();
 
+	      switch (currentPage)
+		  {
+		      case PAGE_MOVE:
+		      {
+		    	  displayPage1(gl);
+		    	  break;
+		      }
+		      
+		      case PAGE_GUN:
+		      {
+		    	  displayPage2(gl);
+		    	  break;
+		      }
+		      
+		      case PAGE_BOMB:
+		      {
+		    	  displayPage3(gl);
+		    	  break;
+		      }
+		      
+		      case PAGE_UTILS:
+		      {
+		    	  displayPage4(gl);
+		    	  break;
+		      }
+	      }
+	       
+	}
+	
+	
+	public void displayPage4(GL10 gl)
+	{
+	      bitmapInstructions4.draw(gl);
+	      
+	      // disable texture + alpha
+	      gl.glDisable( GL10.GL_BLEND );                  // Disable Alpha Blend
+	      gl.glDisable( GL10.GL_TEXTURE_2D );             // Disable Texture Mapping
+
+	      
+	      gl.glMatrixMode( GL10.GL_PROJECTION );          // Activate Projection Matrix
+	      gl.glLoadIdentity();                            // Load Identity Matrix
+	      gl.glOrthof(                                    // Set Ortho Projection (Left,Right,Bottom,Top,Front,Back)
+	         0, screenWidth,
+	         0, screenHeight,
+	         1.0f, -1.0f
+	      );
+	      
+	      gl.glEnable( GL10.GL_TEXTURE_2D );              // Enable Texture Mapping
+	      gl.glEnable( GL10.GL_BLEND );                   // Enable Alpha Blend
+	      gl.glBlendFunc( GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA );  // Set Alpha Blend Function
+
+	      // TEST: render the entire font texture
+	      gl.glColor4f( 1.0f, 1.0f, 1.0f, 1.0f );         // Set Color to Use
+
+	      glText.begin( 1.0f, 1.0f, 1.0f, 1.0f );         // Begin Text Rendering (Set Color WHITE)
+	      int base = screenHeight - (screenHeight / 3 );
+	      int lineHeight = 20;
+	      glText.draw( "To ease the use of the game", 0, base);
+	      glText.draw( "some functions have been", 0, base - lineHeight); 
+	      glText.draw( "introduced to assist you.", 0, base - ( 2 * lineHeight));
+	      glText.end();
+	      
+	      glText.begin( 0.0f, 1.0f, 0.0f, 1.0f );   
+	      glText.draw( "Camera position can be set", 0, base - ( 5 * lineHeight));
+	      glText.draw( "by using the icon on the", 0, base - ( 6 * lineHeight));
+	      glText.draw( "top left. You can either", 0, base - ( 7 * lineHeight));
+	      glText.draw( "turn left or right.", 0, base - ( 8 * lineHeight));
+	      glText.end();
+	      
+	      glText.begin( 1.0f, 0.0f, 0.0f, 1.0f );   
+	      glText.draw( "Zoom can be adjusted by", 0, base - ( 12 * lineHeight));
+	      glText.draw( "by pressing the icons on", 0, base - ( 13 * lineHeight));
+	      glText.draw( "the top right. You can ", 0, base - ( 14 * lineHeight));
+	      glText.draw( "either zoom in or out.", 0, base - ( 15 * lineHeight));
+	      glText.end();  
+	      
+	      
+	      glText.begin( 1.0f, 1.0f, 1.0f, 1.0f );         
+	      glText.draw( "Page 4/4", (screenWidth / 2) - 30, 0);
+	      glText.end(); 
+	      gl.glDisable( GL10.GL_TEXTURE_2D );              // Enable Texture Mapping
+	      gl.glDisable( GL10.GL_BLEND );                   // Enable Alpha Blend
+	}
+	
+	
+	public void displayPage3(GL10 gl)
+	{
+	      bitmapInstructions3.draw(gl);
+	      
+	      // disable texture + alpha
+	      gl.glDisable( GL10.GL_BLEND );                  // Disable Alpha Blend
+	      gl.glDisable( GL10.GL_TEXTURE_2D );             // Disable Texture Mapping
+
+	      
+	      gl.glMatrixMode( GL10.GL_PROJECTION );          // Activate Projection Matrix
+	      gl.glLoadIdentity();                            // Load Identity Matrix
+	      gl.glOrthof(                                    // Set Ortho Projection (Left,Right,Bottom,Top,Front,Back)
+	         0, screenWidth,
+	         0, screenHeight,
+	         1.0f, -1.0f
+	      );
+	      
+	      gl.glEnable( GL10.GL_TEXTURE_2D );              // Enable Texture Mapping
+	      gl.glEnable( GL10.GL_BLEND );                   // Enable Alpha Blend
+	      gl.glBlendFunc( GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA );  // Set Alpha Blend Function
+
+	      // TEST: render the entire font texture
+	      gl.glColor4f( 1.0f, 1.0f, 1.0f, 1.0f );         // Set Color to Use
+
+	      glText.begin( 1.0f, 1.0f, 1.0f, 1.0f );         // Begin Text Rendering (Set Color WHITE)
+	      int base = screenHeight - (screenHeight / 3 );
+	      int lineHeight = 20;
+	      glText.draw( "Cube can also be destroyed", 0, base);
+	      glText.draw( "using bombs. Two different", 0, base - lineHeight); 
+	      glText.draw( "bombs are available: the .", 0, base - ( 2 * lineHeight));
+	      glText.draw( "normal and the big.       ", 0, base - ( 3 * lineHeight));
+	      glText.end();
+	      
+	      glText.begin( 0.0f, 1.0f, 0.0f, 1.0f );   
+	      glText.draw( "Big bombs will explode when", 0, base - ( 5 * lineHeight));
+	      glText.draw( "a cube hit their area but", 0, base - ( 6 * lineHeight));
+	      glText.draw( "will also destroy the ground", 0, base - ( 7 * lineHeight));
+	      glText.draw( "so that the hero might fall", 0, base - ( 8 * lineHeight));
+	      glText.draw( "and die. They can be put on", 0, base - ( 9 * lineHeight));
+	      glText.draw( "ground by pressing the icon", 0, base - ( 10 * lineHeight));
+	      glText.draw( "at the bottom.", 0, base - ( 11 * lineHeight));
+	      glText.end();
+	      
+
+	      glText.begin( 0.0f, 0.0f, 1.0f, 1.0f );   
+	      glText.draw( "Normal bombs will explode when", 0, base - ( 14 * lineHeight));
+	      glText.draw( "a cube hit their area but", 0, base - ( 15 * lineHeight));
+	      glText.draw( "does not destroy the ground", 0, base - ( 16 * lineHeight));
+	      glText.draw( "so that the hero can still", 0, base - ( 17 * lineHeight));
+	      glText.draw( "move in that area. They are", 0, base - ( 18 * lineHeight));
+	      glText.draw( "disposed by pressing the icon", 0, base - ( 19 * lineHeight));
+	      glText.draw( "at the bottom.", 0, base - ( 20 * lineHeight));
+	      glText.end();
+	
+	      
+	      glText.begin( 1.0f, 1.0f, 1.0f, 1.0f );         
+	      glText.draw( "Page 3/4", (screenWidth / 2) - 30, 0);
+	      glText.end(); 
+	      gl.glDisable( GL10.GL_TEXTURE_2D );              // Enable Texture Mapping
+	      gl.glDisable( GL10.GL_BLEND );                   // Enable Alpha Blend
+	}
+	
+	
+	public void displayPage2(GL10 gl)
+	{
+	      bitmapInstructions2.draw(gl);
+	      
+	      // disable texture + alpha
+	      gl.glDisable( GL10.GL_BLEND );                  // Disable Alpha Blend
+	      gl.glDisable( GL10.GL_TEXTURE_2D );             // Disable Texture Mapping
+
+	      
+	      gl.glMatrixMode( GL10.GL_PROJECTION );          // Activate Projection Matrix
+	      gl.glLoadIdentity();                            // Load Identity Matrix
+	      gl.glOrthof(                                    // Set Ortho Projection (Left,Right,Bottom,Top,Front,Back)
+	         0, screenWidth,
+	         0, screenHeight,
+	         1.0f, -1.0f
+	      );
+	      
+	      gl.glEnable( GL10.GL_TEXTURE_2D );              // Enable Texture Mapping
+	      gl.glEnable( GL10.GL_BLEND );                   // Enable Alpha Blend
+	      gl.glBlendFunc( GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA );  // Set Alpha Blend Function
+
+	      // TEST: render the entire font texture
+	      gl.glColor4f( 1.0f, 1.0f, 1.0f, 1.0f );         // Set Color to Use
+
+	      glText.begin( 1.0f, 1.0f, 1.0f, 1.0f );         // Begin Text Rendering (Set Color WHITE)
+	      int base = screenHeight - (screenHeight / 3 );
+	      int lineHeight = 20;
+	      glText.draw( "Cube can disappear by shooting ", 0, base);
+	      glText.draw( "at them. Cube are hitted when", 0, base - lineHeight); 
+	      glText.draw( "you shoot in their direction.", 0, base - ( 2 * lineHeight));
+	      
+	      glText.draw( "Number of remaining bullet are", 0, base - ( 4 * lineHeight));
+	      glText.end();
+	      glText.begin( 1.0f, 0.0f, 0.0f, 1.0f );   
+	      glText.draw( "displayed on top with red.", 0, base - ( 5 * lineHeight));
+	      glText.end();
+	      
+	      glText.begin( 1.0f, 1.0f, 1.0f, 1.0f );
+	      glText.draw( "To shoot a buller, you have to ", 0, base - ( 8 * lineHeight));
+	      glText.end();
+	      glText.begin( 1.0f, 0.0f, 0.0f, 1.0f );         
+	      glText.draw( "press the icon at the bottom.", 0, base - ( 9 * lineHeight));
+	      glText.end();  
+	      
+	      
+	      glText.begin( 1.0f, 1.0f, 1.0f, 1.0f );         
+	      glText.draw( "Page 2/4", (screenWidth / 2) - 30, 0);
+	      glText.end(); 
+	      gl.glDisable( GL10.GL_TEXTURE_2D );              // Enable Texture Mapping
+	      gl.glDisable( GL10.GL_BLEND );                   // Enable Alpha Blend
+	}
+	
+	public void displayPage1(GL10 gl)
+	{
 	      bitmapInstructions1.draw(gl);
 	      
 	      // disable texture + alpha
@@ -174,8 +385,9 @@ public class InstructionsRenderer implements Renderer
 	      glText.begin( 1.0f, 1.0f, 1.0f, 1.0f );         
 	      glText.draw( "Page 1/4", (screenWidth / 2) - 30, 0);
 	      glText.end(); 
-	       
-	}  
+	      gl.glDisable( GL10.GL_TEXTURE_2D );              // Enable Texture Mapping
+	      gl.glDisable( GL10.GL_BLEND );                   // Enable Alpha Blend
+	}
 
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		this.screenWidth = width;
